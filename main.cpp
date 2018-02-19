aedd871257a08a7916445655cfd94caa3f83c73b
tanhlefthander <tanhlefthander@users.noreply.github.com>
2018-02-19 21:12:40 +0700

    new

--- main.cpp
+++ main.cpp
@@ -1,26 +1,12 @@
 #include <iostream>
-#include <string>
-#include "BigInt.h"
+#include "AlgorithmBigInt.h"
 using namespace std;
 int main(){
-    //cout<< BigInt("0") + BigInt("-2")<<endl;
-    /*BigInt a("-10"), b("2");
-    cout<<"a = "<<a<<" b = "<<b<<endl;
-    cout<<"a + b = "<<(a+b)<<endl;
-    cout<<"a - b = "<<(a-b)<<endl;
-    cout<<"a * b = "<<(a*b)<<endl;
-    cout<<"a / b = "<<(a/b)<<endl;
-    cout<<"a % b = "<<(a%b)<<endl;
-    cout<<"a ^ b = "<<(a^b)<<endl;
-    a="3454";
-    b=-234342;
-    cout<<b.toString()<<endl;
-    cout<<a+"344"<<endl;
-    b[3]= '9';
-    cout<<b<<endl;
-    //b.random(2000);
-    //cout<<b<<endl;*/
-    BigInt a("2"), b("10000");
-    cout<<(a^b);
+    string a("9999"), b("111");
+    cout<<add(a,b)<<endl;
+    cout<<sub(a,b)<<endl;
+    cout<<mul(a,b)<<endl;
+    cout<<divide(a,b)<<endl;
+    cout<<pow(a,b)<<endl;
     return 0;
 }
